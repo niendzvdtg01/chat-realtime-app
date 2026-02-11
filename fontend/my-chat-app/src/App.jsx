e09e269095b9daaafd9ad7ca8f9836149dc2ead3
niendzvdtg01 <duynien34@gmail.com>
2026-02-11 16:26:10 +0700

    add:login page

--- fontend/my-chat-app/src/App.jsx
+++ fontend/my-chat-app/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ChatDashBoard from "./pages/ChatDashboard";
+import LoginPage from "./pages/LoginPage";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<ChatDashBoard />} />
+      <Route path="/login" element={< LoginPage />} />
     </Routes>
   );
 }
--- fontend/my-chat-app/src/components/ChatMessage.jsx
+++ fontend/my-chat-app/src/components/ChatMessage.jsx
@@ -1,32 +1,25 @@
 import '../styles/dashboard/chatmessage.scss'
 import userHeadr from '../assets/dashboard/UsserHeader.png'
 import send from '../assets/dashboard/paper-plane-solid-full.svg'
+import { useState } from 'react'
 export const ChatMessage = () => {
+    const [message, setMessage] = useState("");
     return (
         <div className='chat-message'>
-            <div className="border-bottom">
+            <div className="border-bottom d-flex">
                 <div className='user-img'>
                     <img src={userHeadr} alt="user-Header" className='rounded-circle' />
+                </div>
+                <div>
+                    <p>User</p>
                 </div>
             </div>
             <div className='chat-background'>
                 <div className='chat-text'>
-                    <p>LOL</p>
-                    <p>LOL</p>
-                    <p>LOL</p>
-                    <p>LOL</p>
-                    <p>LOL</p>
-                    <p>LOL</p>
-                    <p>LOL</p>
-                    <p>LOL</p>
-                    <p>LOL</p>
-                    <p>LOL</p>
-                    <p>LOL</p>
-                    <p>LOL</p>
-                    <p>LOL</p>
-                    <p>LOL</p>
-                    <p>LOL</p>
-                    <p>LOL</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic deserunt recusandae non nihil quos quam culpa sapiente, atque veniam veritatis neque dolore nostrum necessitatibus molestias, maiores, aperiam voluptatem minima commodi?</p>
+                </div>
+                <div className='my-chat'>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio corporis atque placeat qui laudantium ratione, ab inventore aliquid amet deserunt possimus voluptatem debitis hic quisquam, eos culpa? Voluptatem, necessitatibus! Quisquam!</p>
                 </div>
             </div>
             <div className='chat-input'>
