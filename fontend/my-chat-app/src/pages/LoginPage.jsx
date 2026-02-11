7e4b23224756d9aab014b379dcdb9ebdc76f3d64
niendzvdtg01 <duynien34@gmail.com>
2026-02-11 17:05:51 +0700

    add:login page

--- fontend/my-chat-app/src/pages/LoginPage.jsx
+++ fontend/my-chat-app/src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import '../styles/login/login.scss'
 import { MDBContainer, MDBCol, MDBRow, MDBBtn, MDBIcon, MDBInput, MDBCheckbox } from 'mdb-react-ui-kit';
-
+import logo from '../assets/logo.png'
 function LoginPage() {
 
     return (
@@ -8,7 +8,16 @@ function LoginPage() {
             <MDBContainer fluid className=" h-custom p-0">
 
                 <div className='d-flex custom-style'>
+                    <div className=" bg-primary">
+                        <div className='d-flex p-4 text-white'>
+                            <div style={{ margin: "0 auto", display: "flex" }}>
+                                <img src={logo} alt="logo" style={{ width: "5rem" }} />
+                                <h2 style={{ fontWeight: "bolder" }}>Synchat</h2>
+                            </div>
+                        </div>
+                    </div>
                     <div className='container mt-5 login-form'>
+
                         <MDBRow>
 
                             <MDBCol col='10' md='6'>
@@ -53,5 +62,4 @@ function LoginPage() {
         </div>
     );
 }
-
 export default LoginPage;
