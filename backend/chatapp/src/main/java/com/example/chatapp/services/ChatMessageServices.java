0d64e6629b2c5b2871c19fe9f35aa2f74906620d
niendzvdtg01 <duynien34@gmail.com>
2026-02-28 23:52:05 +0700

    fix: chat message services

--- backend/chatapp/src/main/java/com/example/chatapp/services/ChatMessageServices.java
+++ backend/chatapp/src/main/java/com/example/chatapp/services/ChatMessageServices.java
@@ -10,18 +10,33 @@ import com.example.chatapp.entity.Conversations;
 import com.example.chatapp.jpa.respository.ConversationRespository;
 import com.example.chatapp.mongodb.respository.ChatMessageRespository;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class ChatMessageServices {
-    @Autowired
-    private ChatMessageRespository chatMessageRespository;
-    private ConversationRespository conversationRespository;
 
-    public void saveMessage(MessageDocument messages) {
-        messages.setTimestamp(LocalDateTime.now());
-        chatMessageRespository.save(messages);
-        // Save conversationId into Mysql database
-        Conversations conversations = conversationRespository.findById(messages.getConversationId()).orElseThrow();
-        conversations.setCreateAt(LocalDateTime.now());
-        conversationRespository.save(conversations);
+    private final ChatMessageRespository chatMessageRespository;
+    private final ConversationRespository conversationRespository;
+
+    public ChatMessageServices(
+            ChatMessageRespository chatMessageRespository,
+            ConversationRespository conversationRespository) {
+
+        this.chatMessageRespository = chatMessageRespository;
+        this.conversationRespository = conversationRespository;
+    }
+
+    @Transactional
+    public void saveMessage(MessageDocument message) {
+
+        message.setTimestamp(LocalDateTime.now());
+
+        chatMessageRespository.save(message);
+        Conversations conversation = conversationRespository
+                .findById(message.getConversationId())
+                .orElseThrow(() -> new RuntimeException("Conversation not found"));
+
+        conversation.setCreateAt(LocalDateTime.now());
+        conversationRespository.save(conversation);
     }
 }
