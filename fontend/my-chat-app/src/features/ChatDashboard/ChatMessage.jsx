5b7a8d8db2763d364712b0e379ded22c55a9633e
niendzvdtg01 <duynien34@gmail.com>
2026-03-07 16:49:09 +0700

    add: find messages via conversationId

--- fontend/my-chat-app/src/features/ChatDashboard/ChatMessage.jsx
+++ fontend/my-chat-app/src/features/ChatDashboard/ChatMessage.jsx
@@ -1,44 +1,64 @@
 import '../../styles/dashboard/chatmessage.scss'
 import userHeadr from '../../assets/dashboard/UsserHeader.png'
 import send from '../../assets/dashboard/paper-plane-solid-full.svg'
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useState, useRef } from 'react'
 import { useChat } from '../../hooks/useChat'
 import useInputState from '../../hooks/useInputState'
 import { UserContext } from '../../services/UserService/UserContext'
+
 export const ChatMessage = () => {
+
     const { message, sendMessage } = useChat();
     const { value, onChange, reset } = useInputState();
     const [allMessages, setAllMessages] = useState([]);
     const context = useContext(UserContext);
+
     const userInfo = context.userInfo;
-    const userName = userInfo?.firstName
+    const userName = userInfo?.firstName;
+
+    const messagesEndRef = useRef(null);
+
+    /* Load message lần đầu */
     useEffect(() => {
         if (context?.message?.messageDocuments) {
-            setAllMessages(context.message?.messageDocuments);
+            setAllMessages(context.message.messageDocuments);
         }
-    }, [context?.message])
+    }, [context?.message]);
+
+    /* Khi websocket nhận message mới */
     useEffect(() => {
         if (message?.length > 0) {
-            const lastest = message[message.length - 1];
-            setAllMessages(prev => [...prev, lastest]);
+            const latest = message[message.length - 1];
+            setAllMessages(prev => [...prev, latest]);
         }
-    }, [message])
+    }, [message]);
+
+    /* Auto scroll xuống cuối */
+    useEffect(() => {
+        messagesEndRef.current?.scrollIntoView({
+            behavior: "smooth"
+        });
+    }, [allMessages]);
+
     const handleSend = () => {
-        if (!value) {
+
+        if (!value.trim()) {
             console.log("value is empty!");
             return;
         }
+
         sendMessage({
             conversationId: context?.message?.conversationId,
             sender: userName?.toString(),
             content: value
-        })
+        });
+
         reset();
     }
-    // console.log(allMessages);
-    console.log(context?.message);
+
     return (
         <div className='chat-message'>
+
             <div className="border-bottom d-flex">
                 <div className='user-img'>
                     <img src={userHeadr} alt="user-Header" className='rounded-circle' />
@@ -47,30 +67,45 @@ export const ChatMessage = () => {
                     <p>User</p>
                 </div>
             </div>
+
             <div className='chat-background'>
-                <div className='chat-text'>
-                    <div>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic deserunt recusandae non nihil quos quam culpa sapiente, atque veniam veritatis neque dolore nostrum necessitatibus molestias, maiores, aperiam voluptatem minima commodi?</p>
+
+                {allMessages.map((msg, i) => (
+
+                    <div
+                        key={i}
+                        className={msg.sender === userName?.toString()
+                            ? 'my-chat'
+                            : 'chat-text'}
+                    >
+                        <p>
+                            <b>{msg.sender}:</b> {msg.content}
+                        </p>
+
                     </div>
-                </div>
-                <div className='my-chat'>
-                    {allMessages.map((msg, i) => (
-                        <div key={i}>
-                            <p >
-                                <b>{msg.sender}:</b> {msg.content}
-                            </p>
-                        </div>
-                    ))}
-                </div>
+
+                ))}
+
+                {/* anchor để scroll */}
+                <div ref={messagesEndRef}></div>
+
             </div>
+
             <div className='chat-input'>
                 <div className='chat-input-group'>
-                    <input type="text"
+                    <input
+                        type="text"
                         value={value}
-                        onChange={onChange} />
-                    <button onClick={handleSend}><img src={send} alt="send meesage" /></button>
+                        onChange={onChange}
+                        placeholder="Type message..."
+                    />
+
+                    <button onClick={handleSend}>
+                        <img src={send} alt="send message" />
+                    </button>
                 </div>
             </div>
+
         </div>
     )
-}   
+}
