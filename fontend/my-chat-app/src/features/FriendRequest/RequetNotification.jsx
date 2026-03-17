0335fa0620786756ec78073e5164c70ed1c3b0cf
niendzvdtg01 <duynien34@gmail.com>
2026-03-17 11:27:42 +0700

    add: UI request

--- fontend/my-chat-app/src/features/FriendRequest/RequetNotification.jsx
+++ fontend/my-chat-app/src/features/FriendRequest/RequetNotification.jsx
@@ -1,9 +1,19 @@
 import '../../styles/dashboard/friendrequest.scss'
+import { RequestList } from './ReuqestList'
 export const FriendNotification = (props) => {
+    const User = {
+        name: "Nien",
+        email: "lol@gmail.com"
+    }
     return (
         <>
             <div className="notification-background">
-
+                <div className='d-flex'>
+                    <span style={{ margin: "0 auto" }}>Request List</span>
+                </div>
+                <div className='request'>
+                    <RequestList name={User.name} email={User.email} />
+                </div>
             </div>
         </>
     )
