fe806f958aecab1811653566b12ed15157a4915d
niendzvdtg01 <duynien34@gmail.com>
2026-03-22 23:32:17 +0700

    add: group chat UI

---
+++ fontend/my-chat-app/src/features/GroupChat/FriendsList.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+export const FriendsList = (props) => {
+    const [click, setClick] = useState(false);
+    return (
+        <ol className="list-group">
+            <li className="list-group-item d-flex justify-content-between align-items-start">
+                <div className="ms-2 me-auto">
+                    <div className="fw-bold">{props.name}</div>
+                    {props.email}
+                </div>
+                <button style={{
+                    backgroundColor: click ? "red" : "#0d6efd",
+                    color: "white",
+                    width: "4rem",
+                    height: "2rem",
+                    border: "none",
+                    borderRadius: "20px"
+                }} onClick={() => {
+                    setClick(true)
+                    props.setAddmember()
+                }}
+                >
+                    {click ? "added" : "add"}
+                </button>
+            </li>
+        </ol>
+    )
+}
